87c92a907e057b96615103f396d4ef425a167308
coffee-marios <mark_theo@yahoo.com>
2024-03-20 11:44:57 +0200

    Setting up webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["bundle.js"], // Clean only the 'bundle.js' file
+    }),
+  ],
+};
