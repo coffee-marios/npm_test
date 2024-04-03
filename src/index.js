143cb8da8ba897bfb1aed195bc057bf2177f9d43
coffee-marios <mark_theo@yahoo.com>
2024-04-03 13:04:23 +0300

    Add a function that hides an element when a button is clicked

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./style.css";
+
+function toggleElement() {
+  let element = document.getElementById("buttonColors");
+  element.classList.toggle("hideElement");
+}
+
+let myButton = document.getElementById("buttonEl");
+myButton.addEventListener("click", toggleElement, false);
+
+let topButtons = document.querySelectorAll(".navButton");
+
+topButtons.forEach((topButton) => {
+  topButton.addEventListener("click", function () {
+    for (let i = 0; i < topButtons.length; i++) {
+      topButtons[i].style.backgroundColor = "rgb(226, 188, 53)";
+    }
+
+    topButton.style.backgroundColor = "green";
+  });
+});
+
+export { toggleElement };
