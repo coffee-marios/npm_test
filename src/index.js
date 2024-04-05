0d0d3e32419411bef36b3ca739d5ec9d60141d74
coffee-marios <mark_theo@yahoo.com>
2024-04-05 13:04:48 +0300

    Turn every navigation button into a button that toggles a dropdown menu

--- src/index.js
+++ src/index.js
@@ -5,17 +5,26 @@ function toggleElement() {
   element.classList.toggle("hideElement");
 }
 
-let myButton = document.getElementById("buttonEl");
-myButton.addEventListener("click", toggleElement, false);
-
 let topButtons = document.querySelectorAll(".navButton");
+let dropMenus = document.querySelectorAll(".dropOptions");
+
+const mapNav = new Map();
+for (let i = 0; i < topButtons.length; i++) {
+  mapNav.set(topButtons[i], dropMenus[i]);
+}
+
+console.log(dropMenus[0]);
+
+// Every time we click a button:
+// its color changes;
+// a dropdown menu appears or disappears
 
 topButtons.forEach((topButton) => {
   topButton.addEventListener("click", function () {
     for (let i = 0; i < topButtons.length; i++) {
       topButtons[i].style.backgroundColor = "rgb(226, 188, 53)";
     }
-
+    mapNav.get(topButton).classList.toggle("hideElement");
     topButton.style.backgroundColor = "green";
   });
 });
