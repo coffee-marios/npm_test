f46666c4326d0d650b93c8a2dd974fc47f694ddd
coffee-marios <mark_theo@yahoo.com>
2024-04-06 13:44:30 +0300

    Add an image and configure webpack to show images

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -15,11 +16,29 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "/dist",
+            },
+          },
+        ],
+      },
     ],
   },
+
   plugins: [
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: ["bundle.js"], // Clean only the 'bundle.js' file
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "images", to: "images" }, // Adjust the source and destination paths as needed
+      ],
     }),
   ],
 };
